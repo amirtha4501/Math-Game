994a3925c2f9a957457209569531f205c2096f66
Amirtha <amirtha4501@gmail.com>
2020-05-10 11:42:09 +0530

    Configuring for gh-pages

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'MathGame'`, () => {
+  it(`should have as title 'MathMaster'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('MathGame');
+    expect(app.title).toEqual('MathMaster');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('MathGame app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('MathMaster app is running!');
   });
 });
